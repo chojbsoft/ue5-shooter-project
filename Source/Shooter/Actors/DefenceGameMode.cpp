904e08f7b685c7b821db39ace048c2466de9cbfb
t2ggerch0 <chojbsoft@gmail.com>
2025-03-10 13:46:17 +0900

    Move StageSubsystem to DefenceGameMode

--- Source/Shooter/Actors/DefenceGameMode.cpp
+++ Source/Shooter/Actors/DefenceGameMode.cpp
@@ -3,3 +3,108 @@
 
 #include "Actors/DefenceGameMode.h"
 
+#include <Kismet/GameplayStatics.h>
+#include "Actors/Enemy.h"
+#include "Actors/SplineEnemySpawner.h"
+
+ADefenceGameMode::ADefenceGameMode()
+{
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+void ADefenceGameMode::BeginPlay()
+{
+	Super::BeginPlay();
+	StartStage();
+}
+
+void ADefenceGameMode::StartStage()
+{
+	if (!StageDataAsset || StageDataAsset->Waves.IsEmpty())
+	{
+		SetActorTickEnabled(false);
+		return;
+	}
+	if (IsValid(LevelSequenceActor))
+	{
+		LevelSequenceActor->Destroy();
+		ensure(false);
+	}
+
+	LevelSequenceActor = GetWorld()->SpawnActor<ALevelSequenceActor>();
+	StartWave(0);
+}
+
+void ADefenceGameMode::StartWave(const uint32 WaveIndex)
+{
+	if (WaveIndex >= (uint32)StageDataAsset->Waves.Num())
+	{
+		OnStageFinished();
+		return;
+	}
+
+	bWaveFinished = false;
+	CurrentWaveIndex = WaveIndex;
+	FWaveInfo& WaveInfo = StageDataAsset->Waves[WaveIndex];
+	LevelSequenceActor->SetSequence(WaveInfo.Wave);
+	if (!LevelSequenceActor->GetSequencePlayer()->OnFinished.IsBound())
+	{
+		LevelSequenceActor->GetSequencePlayer()->OnFinished.AddDynamic(this, &ADefenceGameMode::OnWaveFinished);
+	}
+	LevelSequenceActor->GetSequencePlayer()->Play();
+}
+
+void ADefenceGameMode::OnStageFinished()
+{
+	LevelSequenceActor->Destroy();
+	StageDataAsset = nullptr;
+}
+
+void ADefenceGameMode::OnWaveFinished()
+{
+	bWaveFinished = true;
+}
+
+void ADefenceGameMode::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	if (!CanTransitionWave())
+	{
+		return;
+	}
+
+	const uint32 NextWaveIndex = CurrentWaveIndex + 1;
+	StartWave(NextWaveIndex);
+}
+
+bool ADefenceGameMode::CanTransitionWave()
+{
+	if (!bWaveFinished)
+	{
+		return false;
+	}
+
+	FWaveTransitionRule WaveTransitionRule = StageDataAsset->Waves[CurrentWaveIndex].WaveTransitionRule;
+	switch (WaveTransitionRule)
+	{
+	case FWaveTransitionRule::Auto:
+		return true;
+	case FWaveTransitionRule::KillAllEnemies:
+	{
+		AActor* FindSpawner = UGameplayStatics::GetActorOfClass(LevelSequenceActor, ASplineEnemySpawner::StaticClass());
+		if (FindSpawner)
+		{
+			return false;
+		}
+		AActor* FindEnemy = UGameplayStatics::GetActorOfClass(LevelSequenceActor, AEnemy::StaticClass());
+		if (FindEnemy)
+		{
+			return false;
+		}
+	}
+	return true;
+	}
+
+	return false;
+}
--- Source/Shooter/Actors/DefenceGameMode.h
+++ Source/Shooter/Actors/DefenceGameMode.h
@@ -2,16 +2,72 @@
 
 #pragma once
 
+#include "LevelSequenceActor.h"
+#include "LevelSequencePlayer.h"
+
 #include "CoreMinimal.h"
 #include "GameFramework/GameModeBase.h"
 #include "DefenceGameMode.generated.h"
 
-/**
- * 
- */
+UENUM(BlueprintType)
+enum class FWaveTransitionRule : uint8
+{
+	Auto,
+	KillAllEnemies
+};
+
+USTRUCT()
+struct FWaveInfo
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere)
+	ULevelSequence* Wave;
+
+	// 전환 조건
+	UPROPERTY(EditAnywhere)
+	FWaveTransitionRule WaveTransitionRule = FWaveTransitionRule::Auto;
+};
+
+UCLASS(BlueprintType)
+class UStageDataAsset : public UDataAsset
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditAnywhere)
+	TArray<FWaveInfo> Waves;
+};
+
 UCLASS()
 class SHOOTER_API ADefenceGameMode : public AGameModeBase
 {
 	GENERATED_BODY()
-	
+
+public:
+	ADefenceGameMode();
+	virtual void BeginPlay() override;
+
+private:
+	void StartStage();
+	void StartWave(const uint32 WaveIndex);
+
+protected:
+	virtual void Tick(float DeltaTime) override;
+
+protected:
+	bool CanTransitionWave();
+
+	UFUNCTION()
+	void OnWaveFinished();
+	void OnStageFinished();
+
+protected:
+	UPROPERTY(EditAnywhere)
+	UStageDataAsset* StageDataAsset;
+	UPROPERTY(Transient)
+	ALevelSequenceActor* LevelSequenceActor;
+
+	bool bWaveFinished = false;
+	uint32 CurrentWaveIndex = 0;
 };
